793e2c6cda8f0e6ed6ebc29c7697af595996ff1c
Lucas Ferreira <lucasferreira@MacBook-Pro-de-Lucas.local>
2019-07-08 20:45:08 -0300

    gitignore config.

--- caroneiro-backend/src/trip/interfaces/trip.interface.ts
+++ caroneiro-backend/src/trip/interfaces/trip.interface.ts
@@ -1,17 +1,17 @@
 import { Document } from 'mongoose';
 
 export enum RecurrenceTypeEnum {
-    DAILY = 'daily',
-    WEEKLY = 'weekly',
-    MONTHLY = 'monthly',
+  DAILY = 'daily',
+  WEEKLY = 'weekly',
+  MONTHLY = 'monthly',
 }
 
 export interface ITrip extends Document {
-    tripName: string;
-    tripDate: Date;
-    tripCost: number;
-    isSharingCost?: boolean;
-    isRecurrent?: boolean;
-    recurrenceTimes?: number;
-    recurrenceType?: RecurrenceTypeEnum;
+  tripName: string;
+  tripDate: Date;
+  tripCost: number;
+  isSharingCost?: boolean;
+  isRecurrent?: boolean;
+  recurrenceTimes?: number;
+  recurrenceType?: RecurrenceTypeEnum;
 }
