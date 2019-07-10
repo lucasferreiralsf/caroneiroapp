614e30cce2b1bdc3d66d9fd86784910c2bffa7ef
Lucas Ferreira <lucasferreira@MacBook-Pro-de-Lucas.local>
2019-07-09 23:28:07 -0300

    Trips and auth implementation

--- caroneiro-api/src/models/trips.model.js
+++ caroneiro-api/src/models/trips.model.js
@@ -1,15 +1,43 @@
 // trips-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
+module.exports = function(app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
-  const trips = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const trips = new Schema(
+    {
+      tripName: {
+        type: String,
+        required: true
+      },
+      tripDate: {
+        type: Date,
+        required: true
+      },
+      tripCost: Number,
+      isSharingCost: {
+        type: Boolean,
+        default: false
+      },
+      isRecurrent: {
+        type: Boolean,
+        default: false
+      },
+      recurrenceTimes: {
+        type: Number,
+        default: 0
+      },
+      recurrenceType: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly'],
+        default: 'daily'
+      }
+    },
+    {
+      timestamps: true
+    }
+  );
 
   return mongooseClient.model('trips', trips);
 };
--- caroneiro-api/src/services/index.js
+++ caroneiro-api/src/services/index.js
@@ -1,5 +1,7 @@
 const trips = require('./trips/trips.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(trips);
+  app.configure(users);
 };
