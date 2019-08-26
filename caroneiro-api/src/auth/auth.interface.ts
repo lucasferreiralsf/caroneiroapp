7ade5401ae443ca90398dcbccb46bcffa498e464
Lucas Ferreira <lucasferreira@MacBook-Pro-de-Lucas.local>
2019-08-25 21:16:05 -0300

    Init nestjs api v2

--- caroneiro-api/src/auth/auth.interface.ts
+++
@@ -1,6 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export interface IAuth extends mongoose.Document {
-  readonly email: string;
-  readonly password: string;
-}
--- caroneiro-api/src/auth/session-auth.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SessionAuthMiddleware } from './session-auth.middleware';
-
-describe('SessionAuthMiddleware', () => {
-  it('should be defined', () => {
-    expect(new SessionAuthMiddleware()).toBeDefined();
-  });
-});
