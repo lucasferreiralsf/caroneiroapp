e978dab37a20ffdbe9fbfe6aa80413bc76d650c6
lucasferreiralsf <35882258+lucasferreiralsf@users.noreply.github.com>
2019-09-04 17:15:20 -0300

    initMongoProd deleted;

--- initMongoProd/createusers.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating users..."
-mongo admin --host localhost -u rootprod -p HtwR48wi9m3 --eval "db.createUser({user: 'caroneiroadmin', pwd: 'CsA4er78t',roles: [{role: 'dbOwner', db: 'caroneiroapp_prod'}, {role: 'dbOwner', db: 'caroneiroapp_dev'}]}); db.createUser({user: 'administrator', pwd: 'Santos09021992', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
-echo "Users created."
